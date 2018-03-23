26dee1cb1ea04a4733817f4e653edfba85cb095d
Igor Chalo Sales <igor.chalosales@jumbo.com>
2018-03-23 15:42:02 +0100

    Unique message.

--- test/src/com/test/Class2.java
+++ test/src/com/test/Class2.java
@@ -1,5 +1,9 @@
 package com.test;
 
 public class Class2 {
+	
+	public static void main(String[] args) {
+		System.out.println("");
+	}
 
 }
--- test/src/com/test/Class3.java
+++ test/src/com/test/Class3.java
@@ -1,5 +1,9 @@
 package com.test;
 
 public class Class3 {
+	
+	public static void main(String[] args) {
+		
+	}
 
 }
